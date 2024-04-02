da7dc15530b635c7dc3833c2efc8c6b69db56f53
DagmarMpheio <dagmarmpheiu@gmail.com>
2024-04-02 12:24:05 +0100

    metodos do crud

--- backend/app/Http/Controllers/TarefaController.php
+++ backend/app/Http/Controllers/TarefaController.php
@@ -2,9 +2,86 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tarefa;
 use Illuminate\Http\Request;
 
 class TarefaController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $tarefas = Tarefa::all();
+        return response()->json($tarefas);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        //por padrao uma tarefa tem status de pendente
+        $request['status'] = 'Pendente';
+
+        $tarefa = Tarefa::create($request->all());
+        return response()->json($tarefa, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $tarefa = Tarefa::find($id);
+        return response()->json($tarefa);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+        ]);
+
+        //por padrao uma tarefa tem status de pendente
+        $request['status'] = 'Pendente';
+
+        $tarefa = Tarefa::find($id);
+        $tarefa->update($request->all());
+        return response()->json($tarefa, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Tarefa::destroy($id);
+        return response()->json(null, 204);
+    }
 }
