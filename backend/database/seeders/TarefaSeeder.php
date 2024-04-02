ffafdd6341d244068711719251e6c0b90e70a611
DagmarMpheio <dagmarmpheiu@gmail.com>
2024-04-02 12:50:44 +0100

    seeders

--- backend/database/seeders/TarefaSeeder.php
+++ backend/database/seeders/TarefaSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tarefa;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TarefaSeeder extends Seeder
 {
@@ -12,6 +15,20 @@ class TarefaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //reset a tabela tarefas
+        //DB::statement('SET FOREIGN_KEY_CHECKS=0'); // para mysql
+        DB::statement('PRAGMA foreign_keys = OFF;'); // para sqlite
+        DB::table('tarefas')->truncate(); //apagar todos os dados da tabela
+
+        //gerar 5 tarefas
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 5; $i++) {
+            $tarefa = Tarefa::create([
+                'name' => $faker->text(50),
+                'description' => $faker->text(),
+                'status' => 'Pendente',
+            ]);
+        }
     }
 }
